dfbbaa3a723803f26e2738391740f546e179c04c
SayamBar <sayam.bar@bitcanny.com>
2022-09-08 10:13:36 +0530

    second commit

--- linear_search.rb
+++ linear_search.rb
@@ -1,24 +1,14 @@
 class Array
-    def binary_search(a,key)
+    def linear_search(a,key)
         flag = 0
-        n = a.length()
-        low = 0
-        high = n-1
-        mid = (low + high) / 2
-        while low < high
-            if a[mid] < key
-                low = mid + 1
-            elsif a[mid] > key
-                high = mid - 1
-            else
+        a.each do |a|
+            if a == key
                 flag = 1
                 break
             end
-            mid = (low + high) / 2
         end
-        
         if flag == 1
-            puts "#{key} is found at position #{mid+1}"
+            puts "#{key} is found"
         else
             puts "#{key} is not found"
         end
@@ -36,4 +26,4 @@ print "Enter the numbers:"
 end
 print "Enter the key to be searched:"
 key = gets.to_i
-o.binary_search(a,key)
+o.linear_search(a,key)
